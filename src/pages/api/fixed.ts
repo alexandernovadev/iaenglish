a887224b9114d3f62be6121ac531abc0a5f04d54
alexandernovadev <alexsk88.dev@gmail.com>
2023-11-26 23:05:46 -0500

    1400 6% nada

--- src/pages/api/fixed.ts
+++ src/pages/api/fixed.ts
@@ -1,3 +1,4 @@
+import { WORDSHERERAAW } from "@/data/wordsraw";
 import { Word } from "@/interfaces/word";
 import { connect, disconnect } from "@/mongo";
 import { WordModel } from "@/mongo/models/Word";
@@ -14,56 +15,11 @@ export default async function handler(
   try {
     switch (method) {
       case "GET":
-        // Buscar los repetidos con
-        /**
-         * {
-  _id: "$word",
-  count: {
-    $sum: 1,
-  },
-}
-*/
+        // const rta = await searchAndDeleteRepeatedWords();
+        const rta = await wordsNotInMongo();
+        // const rta = await getArrayFromAllWords()
+        res.status(200).json(rta);
 
-        const repeatedWords = await WordModel.aggregate([
-          {
-            $group: {
-              _id: "$word", // Agrupar por la palabra
-              count: { $sum: 1 }, // Contar apariciones
-            },
-          },
-          {
-            $match: {
-              count: { $gt: 1 }, // Filtrar solo las palabras que aparecen más de una vez
-            },
-          },
-          {
-            $sort: { count: -1 }, // Opcional: ordenar por las más repetidas primero
-          },
-        ]);
-        console.log(repeatedWords);
-
-        for (const wordObj of repeatedWords) {
-          const wordInstances = await WordModel.find({ word: wordObj._id });
-
-          // Si hay más de una instancia, elimina todas excepto la primera
-          for (let i = 1; i < wordInstances.length; i++) {
-            await WordModel.deleteOne({ _id: wordInstances[i]._id });
-          }
-        }
-
-        res.status(200).json({ repeatedWords, r: repeatedWords.length });
-
-        // const words = [
-        // ]
-
-        //   // Buscar en mongo Word.word esa lista comparar con la lista de arriba
-        //   // Las palabras que no estén en mongo, agregarlas a un array y devolverlas como respuesta
-        //   const wordsInMongo = await WordModel.find({}).select("word");
-        //   const wordsInMongoArray = wordsInMongo.map((word) => word.word);
-        //   const wordsNotInMongo = words.filter(
-        //     (word) => !wordsInMongoArray.includes(word)
-        // );
-        // res.status(200).json(wordsNotInMongo);
         break;
 
       case "POST":
@@ -71,29 +27,58 @@ export default async function handler(
         const word = await WordModel.create(newWord);
         res.status(201).json(word);
         break;
-
-      case "PUT":
-        const { id } = req.query;
-        const updatedWord: Word = req.body;
-        const updated = await WordModel.findByIdAndUpdate(id, updatedWord, {
-          new: true,
-        });
-        res.status(200).json(updated);
-        break;
-
-      case "DELETE":
-        const { id: deleteId } = req.query;
-        await WordModel.findByIdAndRemove(deleteId);
-        res.status(204).send("Palabra eliminada correctamente.");
-        break;
-
-      default:
-        res.status(405).json({ error: "Método no permitido." });
-        break;
     }
   } catch (error: any) {
     res.status(500).json({ error: "Error en el servidor: " + error.message });
   } finally {
-    await disconnect(); // Cerrar conexión con MongoDB
+    await disconnect();
   }
 }
+
+const getArrayFromAllWords = async () => {
+  const words = await WordModel.find({}).select("word");
+  const wordsArray = words.map((word) => word.word);
+  return wordsArray;
+};
+
+const searchAndDeleteRepeatedWords = async () => {
+  const repeatedWords = await WordModel.aggregate([
+    {
+      $group: {
+        _id: "$word", // Agrupar por la palabra
+        count: { $sum: 1 }, // Contar apariciones
+      },
+    },
+    {
+      $match: {
+        count: { $gt: 1 }, // Filtrar solo las palabras que aparecen más de una vez
+      },
+    },
+    {
+      $sort: { count: -1 }, // Opcional: ordenar por las más repetidas primero
+    },
+  ]);
+  console.log(repeatedWords);
+
+  for (const wordObj of repeatedWords) {
+    const wordInstances = await WordModel.find({ word: wordObj._id });
+
+    // Si hay más de una instancia, elimina todas excepto la primera
+    for (let i = 1; i < wordInstances.length; i++) {
+      await WordModel.deleteOne({ _id: wordInstances[i]._id });
+    }
+  }
+
+  return { repeatedWords, r: repeatedWords.length };
+};
+
+const wordsNotInMongo = async () => {
+  // Buscar en mongo Word.word esa lista comparar con la lista de arriba
+  // Las palabras que no estén en mongo, agregarlas a un array y devolverlas como respuesta
+  const wordsInMongo = await WordModel.find({}).select("word");
+  const wordsInMongoArray = wordsInMongo.map((word) => word.word);
+  const wordsNotInMongo = WORDSHERERAAW.filter(
+    (word) => !wordsInMongoArray.includes(word)
+  );
+  return { wordsNotInMongo, r: wordsNotInMongo.length };
+};
