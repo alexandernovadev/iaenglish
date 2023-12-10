10e2f6e0683f1b740acc6b30dc5605160b749aab
clax1107 <caurintive34@ucatolica.edu.co>
2023-12-10 13:10:46 -0500

    UI changues more

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -20,10 +20,10 @@ export const MainLayout = ({ children }: Props) => {
 
   return (
     <>
-      <div className="w-full h-screen bg-slate-900 text-white overflow-hidden">
-        <div className="flex flex-col h-full overflow-auto">
-          <div className="flex ">
-            <div className="flex gap-4 justify-between p-4">
+      <div className="w-full h-screen bg-slate-800 text-white overflow-hidden">
+        <div className="flex flex-col h-full overflow-auto ">
+          <div className="flex bg-gradient-to-b from-slate-900 via-slate-800 opacity-9  0 to-transparent  ">
+            <div className="flex gap-4 justify-between p-4 ">
               <NextLink href="/dictionary" passHref>
                 <FaBook style={{ fontSize: 32 }} />
               </NextLink>
@@ -39,7 +39,7 @@ export const MainLayout = ({ children }: Props) => {
               </button>
             </div>
           </div>
-          <div className="flex overflow-auto bg-slate-800 pb-16 pt-8 h-full ">
+          <div className="flex overflow-auto bg-slate-800 pb-16 h-full ">
             {children}
           </div>
         </div>
