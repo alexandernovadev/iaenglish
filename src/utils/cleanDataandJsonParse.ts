3e3486ca8ed047fc3a0ca41512394235e55886ba
Alexander.Nova <Alexander.Nova@Contractor.Prometric.com>
2023-12-29 23:48:34 -0500

    Alex - 1010199 hp eslint si que jode

--- src/utils/cleanDataandJsonParse.ts
+++ src/utils/cleanDataandJsonParse.ts
@@ -1,8 +1,7 @@
- export const dataJsonClean = (_data: string) => {
+export const dataJsonClean = (_data: string) => {
   // Remove all text that GPT could say me
   const regex = /{[\s\S]*}/;
   const jsonEncontrado = _data.match(regex);
-  // @ts-ignore
+  // @ts-expect-error wtf
   return JSON.parse(jsonEncontrado[0]);
 };
-
